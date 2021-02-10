515e5112bb917e9c5adfe241d9452334def88049
Taashvi <71920495+Taashvi@users.noreply.github.com>
2021-02-10 20:32:00 +0530

    Add files via upload

---
+++ Cafe.js
@@ -0,0 +1,44 @@
+img="";
+status="";
+
+function preload(){
+    img=loadImage("cafe.jpg");
+}
+
+function draw(){
+    image(img,0,0,600,400);
+    stroke("#05a7b3");
+    strokeWeight(3);
+    noFill();
+    rect(270,30,100,150);
+    strokeWeight(1);
+    textSize(15);
+    fill("#05a7b3");
+    text("Plant 100%",280,50)
+}
+
+function setup(){
+    var canvas=createCanvas(600,400);
+    canvas.center();
+    detecting_model=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting...";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+    detecting_model.detect(img,gotResults);
+}
+
+function gotResults(error,results){
+    if (error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    }
+}
+
+function back(){
+    window.location="index.html";
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+function dog_cat(){
+    window.location="DogandCat.html";
+}
+
+function room(){
+    window.location="Room.html";
+}
+
+function cafe(){
+    window.location="Cafe.html";
+}
+
+function traffic(){
+    window.location="Traffic.html";
+}
+
+function table(){
+    window.location="Table.html";
+}
